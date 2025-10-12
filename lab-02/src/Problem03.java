a7c2ced4a8fff63b5442aaa1b50f4be93b44fb10
asyli <asyliman20@gmail.com>
2025-10-12 19:57:55 +0600

    feat: lab-02 done

---
+++ lab-02/src/Problem03.java
@@ -0,0 +1,13 @@
+public class Problem03 {
+    public static void main(String[] args) {
+        String part1 = "Java is ";
+        String part2 = "coolllll!";
+
+        // В одну строку
+        System.out.println(part1 + part2);
+
+        // В две строки
+        System.out.println(part1);
+        System.out.println(part2);
+    }
+}
---
+++ lab-02/src/Problem04.java
@@ -0,0 +1,9 @@
+public class Problem04 {
+    public static void main(String[] args) {
+        String name = "Asyl";
+        int age = 18;
+        String city = "Bishkek";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, I live in " + city + ".");
+    }
+}
